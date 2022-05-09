0ef8807730337a10862542f371ef98fcbea36868
Marcelo Vichar <marcos.marcelo43@gmail.com>
2022-05-09 10:08:23 -0300

    stylized cards and buttons

--- components/errorContainer/ErrorContainer.vue
+++ components/errorContainer/ErrorContainer.vue
@@ -13,11 +13,18 @@
       <h1 class="text-6xl md:text-left text-center">404 - page not found</h1>
       <p class="text-center">Something went wrong, go back to the home page</p>
       <nuxt-link to="/" class="flex justify-center">
-        <button
-          class="border mb-2 h-12 rounded-md w-3/4 text-white bg-red-500 hover:opacity-80 hover:shadow-md  transition duration-300 ease-in-out"
+        <div class="w-3/4">
+          <button
+          class="border mb-2 h-12 rounded-md w-full text-red-500 hover:text-white hover:bg-red-500 border border-2 border-red-500 hover:shadow-md  transition duration-300 ease-in-out"
           type="submit">
-          Back to home page
+            <div class="flex justify-center items-center gap-4">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
+              </svg>
+              <p>Back to home page</p>
+            </div>
         </button>
+        </div>
       </nuxt-link>
     </div>
   </div>
