2fc7dd9993ff4d22caa5bc84784b5ac6d39be8fa
Marcelo Vichar <marcos.marcelo43@gmail.com>
2022-05-11 10:29:57 -0300

    refactored components in character page and added links in comic titles

--- components/character/CharacterInfosContainer.vue
+++ components/character/CharacterInfosContainer.vue
@@ -1,25 +1,62 @@
 <template>
   <div class="p-2 w-full flex mb-10 mx-auto flex-col bg-white rounded">
-    <div class="flex flex-col gap-2" >
+    <div class="flex flex-col gap-2">
       <HeaderCharacterInfos :character-infos="characterInfos" />
       <DescriptionCharacterInfos :character-infos="characterInfos" />
-      <ComicsCharacterInfos :character-infos="characterInfos" />
-      <SeriesCharacterInfos :character-infos="characterInfos" />
-      <StoriesCharacterInfos :character-infos="characterInfos" />
+      <ListsContainer :infos="comics" :nameInfo="'Comics'" />
+      <lists-container :infos="series" :nameInfo="'Series'" />
+      <lists-container :infos="events" :nameInfo="'Events'" />
     </div>
   </div>
 </template>
 
 <script>
-import HeaderCharacterInfos from '@/components/character/contentContainer/HeaderCharacterInfos.vue'
-import DescriptionCharacterInfos from '@/components/character/contentContainer/DescriptionCharacterInfos.vue'
-import ComicsCharacterInfos from '@/components/character/contentContainer/ComicsCharacterInfos.vue'
-import SeriesCharacterInfos from '@/components/character/contentContainer/SeriesCharacterInfos.vue'
-import StoriesCharacterInfos from '@/components/character/contentContainer/StoriesCharacterInfos.vue'
+import HeaderCharacterInfos from "@/components/character/contentContainer/HeaderCharacterInfos.vue";
+import DescriptionCharacterInfos from "@/components/character/contentContainer/DescriptionCharacterInfos.vue";
+import ListsContainer from "@/components/character/contentContainer/ListsContainer.vue";
+import Character from "@/services/api";
 
 export default {
-  components: { HeaderCharacterInfos, DescriptionCharacterInfos, ComicsCharacterInfos, SeriesCharacterInfos, StoriesCharacterInfos },
+  components: {
+    HeaderCharacterInfos,
+    DescriptionCharacterInfos,
+    ListsContainer,
+  },
   name: "CharacterInfosContainer",
   props: ["characterInfos"],
+  data() {
+    return {
+      comics: [],
+      series: [],
+      events: [],
+    };
+  },
+
+  async fetch() {
+    const id = this.$route.params?.id;
+    await Character.content(id, "comics")
+      .then((res) => {
+        this.comics = res.data?.data?.results;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    await Character.content(id, "series")
+      .then((res) => {
+        this.series = res.data?.data?.results;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    await Character.content(id, "events")
+      .then((res) => {
+        this.events = res.data?.data?.results;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
 };
 </script>
---
+++ components/character/contentContainer/ListsContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    v-if="infos.length"
+    class="flex flex-col w-full h-auto gap-4 cardcharacter rounded-lg overflow-hidden p-6 shadow-md border border-slate-100"
+  >
+    <h2 class="text-2xl sm:text-4xl">{{ nameInfo }}</h2>
+    <div v-for="info in infos" :key="info.id">
+      <a :href="`${info.urls[0].url}`" target="_blank"
+        ><p>{{ info.title }}</p></a
+      >
+    </div>
+  </div>
+  <div
+    v-else
+    class="flex flex-col w-full h-auto gap-4 rounded-lg overflow-hidden p-6 shadow-md border border-slate-100"
+  >
+    <h2 class="text-2xl sm:text-4xl">{{ nameInfo }} unavailable</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListsContainer",
+  props: ["infos", "nameInfo"],
+};
+</script>
